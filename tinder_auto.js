1935aae5d5962a988708979fce39c57bdb9eba3e
永田 <60948386+watashinagata@users.noreply.github.com>
2020-07-06 20:03:46 +0900

    Update tinder_auto.js

--- tinder_auto.js
+++ tinder_auto.js
@@ -1,13 +1,20 @@
 function tinderLike() {
   var min = 5, max = 10;
-  var rand = Math.floor(Math.random() * (max - min + 1) + min);
-  var elem = document.evaluate('//button[@aria-label="いいね！"]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
+  var rand_time = Math.floor(Math.random() * (max - min + 1) + min);
+  var rand_flag = Math.floor(Math.random() * (max - min + 1) + min);
+  var like_elem = document.evaluate('//button[@aria-label="いいね！"]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
+  var unlike_elem = document.evaluate('//button[@aria-label="いいえ"]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
   try{
-    elem.snapshotItem(0).click();
+  	if(rand_flag>7){
+  		like_elem.snapshotItem(0).click();	
+  	}else{
+  		unlike_elem.snapshotItem(0).click();
+  	}
   }catch(e){
     console.log( e.message );
   }
-  setTimeout(tinderLike, rand * 1000);
+  setTimeout(tinderLike, rand_time * 1000);
 }
+
 
 tinderLike()
