cfe92220a5724851cd2269ba4c89f2ceabc651a6
永田 <60948386+watashinagata@users.noreply.github.com>
2020-07-07 00:12:23 +0900

    Update tinder_auto.js

--- tinder_auto.js
+++ tinder_auto.js
@@ -1,9 +1,9 @@
 function tinderLike() {
-  var min = 5, max = 10;
-  var rand_time = Math.floor(Math.random() * (max - min + 1) + min);
-  var rand_flag = Math.floor(Math.random() * (max - min + 1) + min);
-  var like_elem = document.evaluate('//button[@aria-label="いいね！"]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
-  var unlike_elem = document.evaluate('//button[@aria-label="いいえ"]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
+  const min = 5, max = 10;
+  const rand_time = Math.floor(Math.random() * (max - min + 1) + min);
+  const rand_flag = Math.floor(Math.random() * (max - min + 1) + min);
+  const like_elem = document.evaluate('//button[@aria-label="いいね！"]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
+  const unlike_elem = document.evaluate('//button[@aria-label="いいえ"]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
   try{
   	if(rand_flag>7){
   		like_elem.snapshotItem(0).click();	
